c79fc3ae77a5d01b6c73799d48633cf0cc92719f
Vineeth05kumar <Vineeth05kumar>
2024-01-26 13:11:06 +0530

    adding second file

---
+++ firstCode.js
@@ -0,0 +1,11 @@
+// Write your code below:
+const head=document.getElementById('header');
+head.style.backgroundColor='green';
+head.style.borderBottom='4px solid orange';
+const main=document.getElementById('main-heading');
+main.textContent='Fruit World';
+main.style.color='orange';
+const f=document.getElementById('basket-heading');
+f.style.color='green';
+const last=document.getElementById('thanks');
+last.innerHTML='<p>Please visit us again</p>';
